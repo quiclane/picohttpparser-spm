77c518af22c05fe368b83548a26410c16944c33c
quiclane <developer@veridanlayerseven.com>
2026-02-08 23:11:26 +0200

    Fix tools version header; source-based PicoHTTPParser target

--- Package.swift
+++ Package.swift
@@ -1,14 +1,16 @@
+// swift-tools-version:5.9
 import PackageDescription
 let package=Package(
-name:"picohttpparser-spm",
-products:[
-.library(name:"PicoHTTPParser",targets:["PicoHTTPParser"])
-],
-targets:[
-.target(
-name:"PicoHTTPParser",
-path:"Sources/PicoHTTPParser",
-publicHeadersPath:"include"
-)
-]
+  name:"picohttpparser-spm",
+  platforms:[.iOS(.v12),.tvOS(.v12),.watchOS(.v5),.macOS(.v10_15)],
+  products:[
+    .library(name:"PicoHTTPParser",targets:["PicoHTTPParser"])
+  ],
+  targets:[
+    .target(
+      name:"PicoHTTPParser",
+      path:"Sources/PicoHTTPParser",
+      publicHeadersPath:"include"
+    )
+  ]
 )
